23579f855487cb79ce059a03b79d016c346df69f
Ingvild Hella Kværna <s236763@oslomet.no>
2019-09-18 12:20:49 +0200

    fgfgfgfg

---
+++ Models/bane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oppg1.Models
+{
+    public class bane
+    {
+        public int baneID { get; set; }
+        public string Banenavn { get; set; }
+        public List<string> Stasjoner { get; set; }
+    }
+}
