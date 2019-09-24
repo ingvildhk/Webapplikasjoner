1f5ee11fcf0f5462e93e01a1000807a41cefed59
renkjet <47675663+renkjet@users.noreply.github.com>
2019-09-24 02:30:52 -0700

    Opprettet tabellene i DB.cs

--- Models/DB.cs
+++ Models/DB.cs
@@ -8,6 +8,39 @@ using System.Web;
 
 namespace Oppg1.Models
 {
+    public class Bane
+    {
+        [Key]
+        public int BaneID { get; set; }
+        public String Linje { get; set; } // Eks. linje L1, R10 etc. 
+        public virtual List <Togstopp> Togstopp { get; set; }
+    }
+
+    public class Togstopp
+    {
+        [Key]
+        public int TogstoppID { get; set; }
+        public Stasjon Stasjon { get; set; }
+        public virtual List <DateTime> Avgang { get; set;  }
+    }
+
+    public class Stasjon
+    {
+        [Key]
+        public int StasjonsID { get; set; }
+        public String Stasjonsnavn { get; set; }
+    }
+
+    public class Bestilling
+    {
+        [Key]
+        public int BestillingsID { get; set; }
+        public Bane Bane { get; set; }
+        public Stasjon fraStasjon { get; set; }
+        public Stasjon tilStasjon { get; set; }
+        public DateTime Avgang { get; set; }
+        public String Epost { get; set; } // Foreløpig kundeID 
+    }
     public class DB : DbContext
     {
         public DB() : base("name=DB")
