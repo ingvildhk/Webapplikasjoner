d8bd0f7826fb3a3513e2404dfcd6243b3d7637c0
renkjet <47675663+renkjet@users.noreply.github.com>
2019-10-08 04:05:28 -0700

    Logginn view admin, switch

---
+++ Controllers/LoggInnController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Oppg1.Models;
+
+namespace Oppg1.Controllers
+{
+    public class LoggInnController : Controller
+    {
+        // GET: LoggInn
+        public ActionResult LoggInn()
+        {
+            return View("LoggInn");
+        }
+
+        public ActionResult TilbakeTilIndex()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ Models/AdminLoggInn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oppg1.Models
+{
+    public class AdminLoggInn
+    {
+        public String Brukernavn { get; set; }
+        public String Passord { get; set; }
+    }
+}
+
