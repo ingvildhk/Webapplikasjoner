9e46c3ca8a451ffdc1c74d7d72c9cb7f2facea00
renkjet <47675663+renkjet@users.noreply.github.com>
2019-10-10 03:19:07 -0700

    Adda masse views til Admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
 
 namespace Oppg1.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult AdminOversikt()
+        public ActionResult OversiktStasjoner()
+        {
+            return View();
+        }
+
+        public ActionResult OversiktBaner()
+        {
+            return View();
+        }
+
+        public ActionResult EndreStasjon()
+        {
+            return View();
+        }
+
+        public ActionResult SlettStasjon()
+        {
+            return View();
+        }
+
+        public ActionResult EndreBane()
+        {
+            return View();
+        }
+
+        public ActionResult SlettBane()
         {
             return View();
         }
