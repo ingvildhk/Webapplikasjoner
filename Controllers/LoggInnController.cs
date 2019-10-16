20ce0dae18959cb121f90e5e4c250e911dd2b4c5
renkjet <47675663+renkjet@users.noreply.github.com>
2019-10-16 05:08:24 -0700

    .

--- Controllers/LoggInnController.cs
+++ Controllers/LoggInnController.cs
@@ -29,7 +29,7 @@ namespace Oppg1.Controllers
         public ActionResult LoggInn(bruker innlogget)
         {
             bool finnes = _vyBLL.finnBrukerDB(innlogget);
-            // SJekker om innligging ok
+            // Sjekker om innlogging ok
             if (finnes){
                 Session["Innlogget"] = true;
                 return RedirectToAction("OversiktStasjoner", "Admin");
@@ -40,6 +40,12 @@ namespace Oppg1.Controllers
                 return View();
                     
             }
+        }
+
+        public ActionResult LoggUt()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Index");
         }
     }
 }
